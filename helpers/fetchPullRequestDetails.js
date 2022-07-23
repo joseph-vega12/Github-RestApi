9a90bf059bb1c01c1c22709edeb07c87725df973
Joseph Vega <josephvega@Josephs-MacBook-Pro.local>
2022-07-23 16:16:24 -0700

    refactored for cleaner solution

--- helpers/fetchPullRequestDetails.js
+++ helpers/fetchPullRequestDetails.js
@@ -1,12 +1,21 @@
 const fetch = require("node-fetch");
+const URL =
+  "https://api.github.com/repos/expressjs/express/pulls?state=open&per_page=3";
 
 async function fetchPullRequestDetails() {
-  const fetchPullRequests = await fetch(
-    "https://api.github.com/repos/expressjs/express/pulls?state=open&per_page=3"
-  ).then((res) => res.json());
+  return await fetch(URL)
+    .then((pullRequest) => pullRequest.json())
+    .then((pullRequest) => {
+      return fetchCommitDetails(pullRequest);
+    })
+    .catch((error) => {
+      return new Error(error);
+    });
+}
 
-  const result = await Promise.all(
-    fetchPullRequests.map((pullRequest) => {
+async function fetchCommitDetails(fetchedPullRequests) {
+  return await Promise.all(
+    fetchedPullRequests.map((pullRequest) => {
       return fetch(pullRequest.commits_url)
         .then((commits) => commits.json())
         .then((commits) => {
@@ -23,7 +32,6 @@ async function fetchPullRequestDetails() {
         });
     })
   );
-  return result;
 }
 
 module.exports = fetchPullRequestDetails;
