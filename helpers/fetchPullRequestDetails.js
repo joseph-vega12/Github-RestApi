9f61ab7f0b0f22636f00d33b9f27bd3a87f5f2df
Joseph Vega <josephvega@Josephs-MacBook-Pro.local>
2022-07-24 19:31:30 -0700

    configured dotenv to handle and added Bearer token for the api to handle more requests

--- helpers/fetchPullRequestDetails.js
+++ helpers/fetchPullRequestDetails.js
@@ -1,9 +1,12 @@
 const fetch = require("node-fetch");
 const URL =
   "https://api.github.com/repos/expressjs/express/pulls?state=open&per_page=3";
+const options = {
+  headers: { Authorization: "Bearer " + process.env.ACCESS_TOKEN },
+};
 
 async function fetchPullRequestDetails() {
-  const fetchedPullRequests = await fetch(URL);
+  const fetchedPullRequests = await fetch(URL, options);
   const fetchPullRequestsJson = await fetchedPullRequests.json();
   return fetchCommitDetails(fetchPullRequestsJson);
 }
@@ -11,7 +14,7 @@ async function fetchPullRequestDetails() {
 async function fetchCommitDetails(fetchedPullRequests) {
   const promiseResult = await Promise.all(
     fetchedPullRequests.map(async (pullRequest) => {
-      const fetchedCommits = await fetch(pullRequest.commits_url);
+      const fetchedCommits = await fetch(pullRequest.commits_url, options);
       const fetchedCommitsJson = fetchedCommits.json();
       return {
         id: pullRequest.id,
